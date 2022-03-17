df8c78cc6783d7cd72fef06e6b234050a50117e8
caobandoc <obandocarlosandres@gmail.com>
2022-03-17 14:26:34 -0500

    Separación de responsabilidades con express.Router

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-//para crear datos rapidos
-const faker = require('faker');
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
 
@@ -13,58 +13,7 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  const products = [];
-  const {size} = req.query;
-  const limit = size || 10;
-  for(let i = 0; i < limit; i++){
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl()
-    })
-  }
-  res.json(products);
-});
-
-// los endpoints especificos deben ir al inicio
-app.get('/products/filter', (req, res) => {
-  res.send('Yo soy un filtro');
-});
-
-// los endpoints dinamicos deben ir despues de los especificos
-//http://localhost:3000/products/12
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id: id,
-    name: 'Producto 1',
-    price: 1000
-    });
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset
-    });
-  }else{
-    res.send('No hay parametros');
-  }
-
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId: categoryId,
-    productId: productId,
-    name: 'Producto 1',
-    price: 1000
-    });
-});
+routerApi(app);
 
 
 app.listen(port, () => {
---
+++ routes/products.router.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const faker = require('faker');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const products = [];
+  const {size} = req.query;
+  const limit = size || 10;
+  for(let i = 0; i < limit; i++){
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl()
+    })
+  }
+  res.json(products);
+});
+
+// los endpoints especificos deben ir al inicio
+router.get('/filter', (req, res) => {
+  res.send('Yo soy un filtro');
+});
+
+// los endpoints dinamicos deben ir despues de los especificos
+//http://localhost:3000/products/12
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id: id,
+    name: 'Producto 1',
+    price: 1000
+    });
+});
+
+module.exports = router;
