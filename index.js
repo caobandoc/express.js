dc82e17e70d9cc693214d9214faa55024dd5b344
caobandoc <obandocarlosandres@gmail.com>
2022-03-18 20:52:30 -0500

    POST: método para crear

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const routerApi = require('./routes');
 const app = express();
 const port = 3000;
 
+//middlewares
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
